a937a5dca1da5b3fdf9224c776fdf2e8d5f84a5b
swetaG <sweta.gabhane.31.3@mountblue.tech>
2025-02-21 14:41:37 +0530

    dropdown fixed

--- src/Component/Selectmenu.jsx
+++ src/Component/Selectmenu.jsx
@@ -1,38 +1,31 @@
 const Selectmenu = ({ region, onSelectRegion, theme }) => {
-
   function handleChange(e) {
     onSelectRegion(e.target.value);
   }
   return (
     <div className="mt-10 ml-5   mb-10 lg:-mr-5 xl:-mr-4 2xl:-mr-9 ">
-      <div
-        className={`flex px-5 py-3 w-[12rem] cursor-pointer shadow-md rounded-lg ${
+      <select
+        className={`pl-2  text-gray-600 text-base shadow-md rounded-lg block w-[12rem] h-[3rem] focus:outline-none cursor-pointer ${
           theme === "dark"
-            ? "bg-gray-800 text-white "
-            : "bg-white text-black "
+            ? "bg-gray-800 text-white"
+            : "bg-white text-[hsl(200, 15%, 8%)]"
         }`}
+        onChange={handleChange}
       >
-        <select
-          className={` pl-2 text-gray-600 text-base rounded-lg block w-full  focus:outline-none cursor-pointer ${
-            theme === "dark" ? "bg-gray-800 text-white" : "bg-white text-[hsl(200, 15%, 8%)]"
-          }`}
-          onChange={handleChange}
-        >
-          <option >Filter by Region</option>
-          {region.map((regionArr) => (
-            <option
-              key={regionArr}
-              className={` ${
-                theme === "dark"
-                  ? "bg-gray-700 text-white"
-                  : "bg-white text-[hsl(200, 15%, 8%)]"
-              }`}
-            >
-              {regionArr}
-            </option>
-          ))}
-        </select>
-      </div>
+        <option>Filter by Region</option>
+        {region.map((regionArr) => (
+          <option
+            key={regionArr}
+            className={` ${
+              theme === "dark"
+                ? "bg-gray-700 text-white"
+                : "bg-white text-[hsl(200, 15%, 8%)]"
+            }`}
+          >
+            {regionArr}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
